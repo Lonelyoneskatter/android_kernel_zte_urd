a52d646c2fb3f85eba9e32a20b308c0409e63408
spart4nicus <paradelovhristiyan@gmail.com>
2019-06-16 16:43:27 +0200

    PM / devfreq: Boost cpubw/gpubw on screen events
    Signed-off-by: spart4nicus <paradelovhristiyan@gmail.com>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -46,6 +46,8 @@ static DEFINE_MUTEX(devfreq_list_lock);
 /* List of devices to boost when the screen is woken */
 static const char *boost_devices[] = {
 	"qcom,cpubw.30",
+	"qcom,cpubw.32",
+	"qcom,gpubw.69",
 };
 
 #define WAKE_BOOST_DURATION_MS (5000)
